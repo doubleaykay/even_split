e6c212b54e4d222c0f15cf44a68972a1184aaeaa
doubleaykay <the.a.cars@gmail.com>
2022-06-04 20:55:28 -0400

    temporarily fix how results are displayed

--- ui.js
+++ ui.js
@@ -11,28 +11,35 @@ function addAnotherPersonToFrontend() {
 // portionOf (h) for each person
 // function to read fields from the Bill object
 function displayResults(people, bill) {
-    resultsDiv = document.getElementById("results")
-    br = document.createElement("br")
+    results_string = "subtotal: " + bill.preTaxTotal + "<br>" + "tax: " + bill.taxAmt + "<br>" + "post-tax: " + bill.postTaxTotal + "<br>" + "tip: " + bill.tipAmt + "<br>" + "tip percent pre-tax: " + bill.tipPctPreTax + "<br>" + "tip percent post-tax: " + bill.tipPctPostTax + "<br>" + "TOTAL: " + bill.postTipTotal + "<br>"
 
-    resultsDiv.appendChild(document.createTextNode("subtotal: " + bill.preTaxTotal + "\n"))
-    resultsDiv.appendChild(br)
-    resultsDiv.appendChild(document.createTextNode("tax: " + bill.taxAmt + "\n"))
-    resultsDiv.appendChild(br)
-    resultsDiv.appendChild(document.createTextNode("post-tax: " + bill.postTaxTotal + "\n"))
-    resultsDiv.appendChild(br)
-    resultsDiv.appendChild(document.createTextNode("tip: " + bill.tipAmt + "\n"))
-    resultsDiv.appendChild(br)
-    resultsDiv.appendChild(document.createTextNode("tip percent pre-tax: " + bill.tipPctPreTax + "\n"))
-    resultsDiv.appendChild(br)
-    resultsDiv.appendChild(document.createTextNode("tip percent post-tax: " + bill.tipPctPostTax + "\n"))
-    resultsDiv.appendChild(br)
-    resultsDiv.appendChild(document.createTextNode("TOTAL: " + bill.postTipTotal + "\n"))
-    resultsDiv.appendChild(br)
+    resultsDiv = document.getElementById("results")
+    // br = document.createElement("br")
+
+    // resultsDiv.appendChild(document.createTextNode("subtotal: " + bill.preTaxTotal + "\n"))
+    // resultsDiv.appendChild(br)
+    // resultsDiv.appendChild(document.createTextNode("tax: " + bill.taxAmt + "\n"))
+    // resultsDiv.appendChild(br)
+    // resultsDiv.appendChild(document.createTextNode("post-tax: " + bill.postTaxTotal + "\n"))
+    // resultsDiv.appendChild(br)
+    // resultsDiv.appendChild(document.createTextNode("tip: " + bill.tipAmt + "\n"))
+    // resultsDiv.appendChild(br)
+    // resultsDiv.appendChild(document.createTextNode("tip percent pre-tax: " + bill.tipPctPreTax + "\n"))
+    // resultsDiv.appendChild(br)
+    // resultsDiv.appendChild(document.createTextNode("tip percent post-tax: " + bill.tipPctPostTax + "\n"))
+    // resultsDiv.appendChild(br)
+    // resultsDiv.appendChild(document.createTextNode("TOTAL: " + bill.postTipTotal + "\n"))
+    // resultsDiv.appendChild(br)
 
     people.forEach(e => {
-        resultsDiv.appendChild(document.createTextNode(e.name + " pays $" + e.contributionAmt + "\n"))
-        resultsDiv.appendChild(br)
+        // resultsDiv.appendChild(document.createTextNode(e.name + " pays $" + e.contributionAmt + "\n"))
+        // resultsDiv.appendChild(br)
+
+        results_string = results_string + e.name + " pays $" + e.contributionAmt + "<br>"
     })
+
+    resultsDiv.innerHTML = results_string
+    console.log(results.string)
     // console.log("Dan contribution: " + personList[0].contributionAmt)
     // console.log("Anoush contribution: " + personList[1].contributionAmt)
 }
